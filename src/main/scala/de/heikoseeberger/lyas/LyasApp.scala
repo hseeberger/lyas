6e0d1f04199e2f29db398de4fcd9ac7db130ac81
Heiko Seeberger <mail@heikoseeberger.de>
2016-07-21 18:57:48 +0200

    Prepare some code (groll:initial)

---
+++ src/main/scala/de/heikoseeberger/lyas/LyasApp.scala
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2016 Heiko Seeberger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.heikoseeberger.lyas
+
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.{ Keep, Sink, Source }
+import akka.stream.{ ActorMaterializer, ThrottleMode }
+import scala.concurrent.Await
+import scala.concurrent.duration.{ Duration, DurationInt }
+
+object LyasApp {
+
+  final val Title =
+    """| _                                          _   _   _          ___ _
+       || |   ___ __ _ _ _ _ _    _  _ ___ _  _    /_\ | |_| |____ _  / __| |_ _ _ ___ __ _ _ __  ___
+       || |__/ -_) _` | '_| ' \  | || / _ \ || |  / _ \| / / / / _` | \__ \  _| '_/ -_) _` | '  \(_-<
+       ||____\___\__,_|_| |_||_|  \_, \___/\_,_| /_/ \_\_\_\_\_\__,_| |___/\__|_| \___\__,_|_|_|_/__/
+       |                          |__/
+       |  __                             _      ___              _ _
+       | / _|___ _ _   __ _ _ _ ___ __ _| |_   / __|___  ___  __| | |
+       ||  _/ _ \ '_| / _` | '_/ -_) _` |  _| | (_ / _ \/ _ \/ _` |_|
+       ||_| \___/_|   \__, |_| \___\__,_|\__|  \___\___/\___/\__,_(_)
+       |              |___/
+       |""".stripMargin
+
+  def main(args: Array[String]): Unit = {
+    println(Title)
+
+    implicit val system = ActorSystem()
+    implicit val mat    = ActorMaterializer()
+    import system.dispatcher
+
+    val lyas = Source
+      .repeat("Learn you Akka Streams for great Good!")
+      .zip(Source.fromIterator(() => Iterator.from(0)))
+      .take(7)
+      .mapConcat {
+        case (s, n) =>
+          val i = " " * n
+          f"$i$s%n"
+      }
+      .throttle(42, 500.millis, 1, ThrottleMode.Shaping)
+      .toMat(Sink.foreach(print))(Keep.right)
+
+    val done = lyas.run()
+    done.onComplete(_ => system.terminate())
+
+    Await.ready(system.whenTerminated, Duration.Inf)
+  }
+}
---
+++ src/test/scala/de/heikoseeberger/lyas/SseServerSpec.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2016 Heiko Seeberger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.heikoseeberger.lyas
+
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.client.RequestBuilding
+import akka.http.scaladsl.model.StatusCodes.{ BadRequest, OK }
+import akka.http.scaladsl.model.headers.Accept
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import de.heikoseeberger.akkasse.MediaTypes.`text/event-stream`
+import de.heikoseeberger.akkasse.{ EventStream, ServerSentEvent }
+import de.heikoseeberger.akkasse.client.EventStreamUnmarshalling
+import de.heikoseeberger.akkasse.headers.`Last-Event-ID`
+
+class SseServerSpec extends BaseSpec {
+  import EventStreamUnmarshalling._
+  import RequestBuilding._
+
+  private final val Address = "localhost"
+
+  private final val Text = "Learn you Akka Streams for great Good!"
+
+  "SseServer" should {
+    "respond to GET / with an appropriate source of server-sent events" in {
+      val port        = 10001
+      val lastEventId = 10
+      SseServer(Address, port, 100)
+
+      Http()
+        .singleRequest(request(port, lastEventId.toString))
+        .flatMap { response =>
+          response.status shouldBe OK
+          response.entity.contentType.mediaType shouldBe `text/event-stream`
+          Unmarshal(response)
+            .to[EventStream]
+            .flatMap(_.runFold("")(_ + _.data.get))
+        }
+        .map(_ shouldBe Text.substring(lastEventId + 1))
+    }
+
+    "respond to GET / with an illegal last event id header with BadRequest" in {
+      val port = 10002
+      SseServer(Address, port, 100)
+
+      Http()
+        .singleRequest(request(port, "abc"))
+        .map(_.status shouldBe BadRequest)
+    }
+  }
+
+  private def request(port: Int, lastEventId: String) =
+    Get(s"http://$Address:$port")
+      .withHeaders(Accept(`text/event-stream`), `Last-Event-ID`(lastEventId))
+}
