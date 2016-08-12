db38f6368d806561a084b810d59339554da0118a
Heiko Seeberger <mail@heikoseeberger.de>
2016-08-12 14:40:19 +0200

    Add LastElement stage

--- src/main/scala/de/heikoseeberger/lyas/SseClient.scala
+++ src/main/scala/de/heikoseeberger/lyas/SseClient.scala
@@ -105,7 +105,12 @@ object SseClient {
           send(request)
             .flatMap(Unmarshal(_).to[Source[ServerSentEvent, Any]])
             .fallbackTo(noEvents)
-        Source.fromFuture(events).flatMapConcat(identity).runWith(handler)
+        Source
+          .fromFuture(events)
+          .flatMapConcat(identity)
+          .viaMat(new LastElement)(Keep.right)
+          .toMat(handler)(Keep.both)
+          .run()
       }
       Flow[Option[String]].map(getAndHandle)
     }
@@ -115,7 +120,16 @@ object SseClient {
       ???
 
     // Graph with shape SourceShape[A]
-    Source.single(lastEventId).via(getAndHandleEvents)
+    Source.fromGraph(GraphDSL.create() { implicit builder =>
+      import GraphDSL.Implicits._
+      val trigger = builder.add(Source.single(lastEventId))
+      val unzip   = builder.add(Unzip[Future[Option[ServerSentEvent]], A])
+      // format: OFF
+      trigger ~> getAndHandleEvents ~> unzip.in
+                                       unzip.out0 ~> Sink.ignore
+      // format: ON
+      SourceShape(unzip.out1)
+    })
   }
 }
 
